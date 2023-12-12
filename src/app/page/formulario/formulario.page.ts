8653139e607a2ee49973454798f20f838570adfe
Inma Caleron <inmacale@gmail.com>
2023-12-12 14:03:11 +0100

    subida ultima

--- src/app/page/formulario/formulario.page.ts
+++ src/app/page/formulario/formulario.page.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ValidationErrors } from '@angular/forms';
+import { Validators, FormControl } from '@angular/forms';
+import { ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.page.html',
   styleUrls: ['./formulario.page.scss'],
 })
-export class FormularioPage implements OnInit {
+export class FormularioPage {
 
-  constructor() { }
+  registerForm: FormGroup = this.formBuilder.group({});
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.createForm();
   }
 
+  private createForm(): void {
+    this.registerForm = this.formBuilder.group({
+      selectCountry: new FormControl('', [Validators.required]),
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl(''),
+      company: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required]),
+      termsConditions: new FormControl(false, [Validators.required]),
+      privacyPolicy: new FormControl(false, [Validators.required])
+
+    })
+  }
 }
