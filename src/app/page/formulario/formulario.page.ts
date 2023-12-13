45e28daa290a71a2ad6d24c64a8bca3991a1b7a3
inma <inmacale@gmail.com>
2023-12-13 20:51:17 +0100

    ejemplo de keyserros

--- src/app/page/formulario/formulario.page.ts
+++ src/app/page/formulario/formulario.page.ts
@@ -12,6 +12,7 @@ import { debounce, debounceTime } from 'rxjs';
 export class FormularioPage {
 
   registerForm: FormGroup = this.formBuilder.group({});
+  keyMessage: string[] | undefined;
 
 
   constructor(private formBuilder: FormBuilder) {
@@ -28,12 +29,53 @@ export class FormularioPage {
       email: new FormControl('', [Validators.email]),
       termsConditions: new FormControl(false, [Validators.requiredTrue]),
       privacyPolicy: new FormControl(false, [Validators.requiredTrue])
-
     });
 
- 
+
+    this.registerForm.valueChanges.pipe(debounceTime(1000)).subscribe(() => {
+      this.generateKeyErrors();
+    });
+
+    
   }
 
- 
+  private generateKeyErrors() {
+    console.log(this.registerForm);
+    const keyErrors:string[] = [];
+    Object.keys(this.registerForm.controls).forEach(controlName => {
+      const control = this.registerForm.get(controlName);
+      if(control && control.errors) {
+        switch(controlName) {
+          case 'selectCountry':
+            if(control.errors?.['required']) {
+              console.log('entro');
+              keyErrors.push('error.selectCounry.required');
+            }
+            break;
+          case 'firstName':
+            if(!control.pristine && control.errors?.['required']) {
+              keyErrors.push('error.firstName.required');
+            }
+            break;
+          case 'lastName':
+            if(!control.pristine && control.errors?.['required']) {
+              keyErrors.push('error.lastName.required');
+            }
+            break;
+          case 'company':
+            if(!control.pristine && control.errors?.['required']) {
+              keyErrors.push('error.company.required');
+            }
+            break;
+        }
+      }
+    });
+    this.keyMessage = keyErrors;
+    console.log(this.keyMessage);
+  }
 
 }
+
+ 
+
+
