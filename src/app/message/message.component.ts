86eb4387b8ff7d50b0b20e60596ebbcbc1fb7bf0
Inma Caleron <inmacale@gmail.com>
2023-12-13 13:52:54 +0100

    subida

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -8,9 +8,32 @@ import { FormGroup } from '@angular/forms';
 })
 export class MessageComponent {
 
-  @Input() genericForm: FormGroup;
-
+  @Input() genericForm: FormGroup | undefined;
+  formErrors: any[] = [];
   constructor() { }
+
+  private getFormErrors(): any[] {
+    const errors: any[] = [];
+    if (this.genericForm) {
+      // Recorre los controles del formulario
+      Object.keys(this.genericForm.controls).forEach(controlName => {
+        const control = this.genericForm!.get(controlName);
+
+        // Verifica si el control tiene errores
+        if (control && control.errors) {
+          // Agrega el objeto completo de errores al array
+          errors.push({
+            control: controlName,
+            errors: control.errors
+          });
+        }
+      });
+    }
+
+
+    // Devuelve el array de objetos de errores
+    return errors;
+  }
 
 
 
