bd0a71679229f32b71d53016d31f4e7275e032dc
Inma Caleron <inmacale@gmail.com>
2023-12-18 13:41:36 +0100

    subidas mensajes funcionando

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -16,6 +16,10 @@ export class MessageComponent implements OnInit {
   visibleCorrect: boolean = false;
   visibleWarning: boolean = false;
 
+  incorrectFields: string[] | undefined;
+  WarningFields: string[] | undefined;
+
+
   constructor() {
 
 
@@ -34,20 +38,69 @@ export class MessageComponent implements OnInit {
   public correctForm(): void {
     console.log(this.genericForm)
     if (this.genericForm?.valid && this.genericForm.touched) {
-      if (this.genericForm.getRawValue().company !== "") {
+      if (this.genericForm.getRawValue().selectCountry !== "" && this.genericForm.getRawValue().company !== "") {
         this.visibleCorrect = true;
         this.visibleIncorrect = false;
         this.visibleWarning = false;
       } else {
-        this.visibleWarning = true;
+        this.buildMessageWarning();
         this.visibleIncorrect = false;
         this.visibleCorrect = false;
 
       }
     } else {
-      this.visibleIncorrect = true;
+      this.buildMessageIncorrect();
+      this.visibleCorrect = false;
+      this.visibleWarning = false;
     }
   }
 
+  public buildMessageIncorrect() {
+
+    const fields: string[] = [];
+    this.visibleIncorrect = true;
+    if (this.genericForm?.get('firstName')?.invalid) {
+      fields.push('First Name es requerido');
+    }
+    if (this.genericForm?.get('lastName')?.invalid) {
+      fields.push('Last Name es requerido');
+    }
+    if (this.genericForm?.get('email')?.hasError('required')) {
+      fields.push('email es requerido');
+    }
+    if (this.genericForm?.get('email')?.hasError('email')) {
+      fields.push('email no valido');
+    }
+
+    if (this.genericForm?.get('phone')?.hasError('required')) {
+      fields.push('phone es requerido');
+    }
+    if (this.genericForm?.get('phone')?.hasError('pattern')) {
+      fields.push('phone no valido');
+    }
+
+    if (this.genericForm?.get('termsConditions')?.invalid) {
+      fields.push('los terminos y condiciones es requerido');
+    }
+    if (this.genericForm?.get('privacyPolicy')?.invalid) {
+      fields.push('la politica de privacidad es requerido');
+    }
+    this.incorrectFields = fields;
+  }
+
+  public buildMessageWarning() {
+
+    const fields: string[] = [];
+    this.visibleWarning = true;
+    if (this.genericForm?.getRawValue().selectCountry == "") {
+      fields.push('select country');
+    }
+
+    if (this.genericForm?.getRawValue().company == "") {
+      fields.push('company');
+    }
+    console.log(fields)
+    this.WarningFields = fields;
+  }
 
 }
